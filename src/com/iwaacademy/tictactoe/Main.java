6621a761f5238497bc5889d3d80d7345a05c453e
Adrian <130883450+Adreamsa@users.noreply.github.com>
2024-10-25 13:08:54 -0600

    feat(main): add method to handle invalid number input
    Added a method in the main class to prevent the program from failing
    when an invalid number is entered.

--- src/com/iwaacademy/tictactoe/Main.java
+++ src/com/iwaacademy/tictactoe/Main.java
@@ -3,23 +3,23 @@ package com.iwaacademy.tictactoe;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String args[]){
+    public static void main(String args[]) {
         int size;
         int gameType;
 
         InitialMenu menu = new InitialMenu();
-        size= menu.selectBoardSize().getBoardSize();
+        size = menu.selectBoardSize().getBoardSize();
         gameType = menu.selectGameType().getValueGameType();
-        Board board = new Board(size,gameType);
+        Board board = new Board(size, gameType);
     }
 
-    static class InitialMenu{
+    static class InitialMenu {
         private Scanner scanner = new Scanner(System.in);
         private EnumBoardSize selectedBoardSize = null;
         private EnumGameType selectedGameType = null;
 
 
-        private EnumBoardSize selectBoardSize(){
+        private EnumBoardSize selectBoardSize() {
 
             while (selectedBoardSize == null) {
                 System.out.println("Select board size::");
@@ -27,28 +27,26 @@ public class Main {
                     System.out.println(size.ordinal() + 1 + ". " + size.getDescription());
                 }
 
-                System.out.print("Enter your option (1-" + EnumBoardSize.values().length + "): "); //Request "board size" option from the user
-                int option1 = scanner.nextInt();
+                int option1 = receiveANumber("Enter your option (1-" + EnumBoardSize.values().length + "): ");
 
                 if (option1 >= 1 && option1 <= EnumBoardSize.values().length) {//Validates that the selected option exists in the ENUM
                     selectedBoardSize = EnumBoardSize.values()[option1 - 1];
                     System.out.println("You have selected: " + selectedBoardSize.getDescription() + " with size " + selectedBoardSize.getBoardSize());
                 } else {
-                    System.out.println("Opción inválida. Intente de nuevo.");
+                    System.out.println("Invalid option. Try again.");
                 }
             }
             return selectedBoardSize;
         }
 
-        private EnumGameType selectGameType(){
-            while(selectedGameType == null){
+        private EnumGameType selectGameType() {
+            while (selectedGameType == null) {
                 System.out.println("Select game type:");
-                for(EnumGameType typeGame : EnumGameType.values()){
-                    System.out.println(typeGame.ordinal() + 1 +". " + typeGame.getDescriptionGameType());
+                for (EnumGameType typeGame : EnumGameType.values()) {
+                    System.out.println(typeGame.ordinal() + 1 + ". " + typeGame.getDescriptionGameType());
                 }
 
-                System.out.print("Enter your option(1-" + EnumBoardSize.values().length + "): ");
-                int option2 = scanner.nextInt();
+                int option2 = receiveANumber("Enter your option(1-" + EnumBoardSize.values().length + "): ");
 
                 if (option2 >= 1 && option2 <= EnumGameType.values().length) {
                     selectedGameType = EnumGameType.values()[option2 - 1];
@@ -58,6 +56,16 @@ public class Main {
                 }
             }
             return selectedGameType;
+        }
+
+        private int receiveANumber(String msg) {
+            System.out.print(msg);
+
+            while (!scanner.hasNextInt()) {
+                System.out.println("Invalid option. Try again. " + msg);
+                scanner.next();
+            }
+            return scanner.nextInt();
         }
     }
 }
